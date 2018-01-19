22e2ee6e8549b81344318cad99fa579ee8c0753e
jessbradford <jbradford@kyvis.com>
2018-01-19 11:49:53 -0700

    add react

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, '/Project/GitHub/CountToTen/src'),
+  context: path.join(__dirname, 'src'),
   entry: [
     './main.js',
   ],
